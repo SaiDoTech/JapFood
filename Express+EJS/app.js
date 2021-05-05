9dc870f9466eb0b5ec2ce162baa17d902c16d5f1
Yaroslav Sidorkevich <72098175+SaiDoTech@users.noreply.github.com>
2021-05-05 18:09:36 +0300

    First lab

--- Express+EJS/app.js
+++ Express+EJS/app.js
@@ -1,38 +1,106 @@
-class Book{
-    constructor(title, author, year)
-    {
-        this.title = title;
-        this.author = author;
-        this.year = year;
-    }
-}
-
-// include express and create an app object
+// подключение express
+const mysql = require("mysql2");
 const express = require("express");
+const bodyParser = require("body-parser");
+const multer  = require("multer");
+const upload = multer({dest:"uploads"});
+
+// создаем объект приложения
 const app = express();
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+
+const pool = mysql.createPool({
+    connectionLimit: 5,
+    host: "localhost",
+    user: "root",
+    database: "japfood",
+    password: "admin"
+  });
+
 app.set("view engine", "ejs");
-app.set('views', 'C:/Users/SAI-HOME/Desktop/Projects/UrBook/Express+EJS/views');
+app.use("/public", express.static(__dirname + "/public"));
+app.use("/uploads", express.static(__dirname + "/uploads"));
 
-// module for reading
-const fs = require("fs");
-const jsonParser = express.json();
-const filePath = __dirname + '/books.json';
-
-app.use("/", function(request, response){
-    const content = fs.readFileSync(filePath,"utf8");
-    const MyBooks = JSON.parse(content);
-
-    response.render("urlist",{
-        books: MyBooks
+// получение списка заказов
+app.get("/", function(req, res){
+    pool.query("SELECT * FROM _order", function(err, data) {
+      if(err) return console.log(err);
+      res.render("index", {
+          orders: data
+      });
     });
 });
 
-app.use(function (request, response) {
-    response.sendStatus(404)
+// возвращаем форму для добавления данных
+app.get("/create", function(req, res){
+    res.render("create");
 });
 
-// начинаем прослушивать подключения на 3000 порту
-app.listen(3000)
-{
-    console.log("Сервер начал прослушивание запросов на порту 3000");
-};
+// получаем отправленные данные и добавляем их в БД 
+app.post("/create", urlencodedParser, upload.single("filedata"), function (req, res) {
+         
+    if(!req.body) return res.sendStatus(400);
+    const date = req.body.date;
+    const client = req.body.client;
+    const status = req.body.status;
+    const sum = req.body.sum;
+
+    let fileinfo = req.file;
+    let file = "";
+
+    if (fileinfo)
+      file = fileinfo.path;
+
+    pool.query("INSERT INTO _order (o_date, o_client, o_status, o_sum, o_file) VALUES (?,?,?,?,?)", [date, client, status, sum, file], function(err, data) {
+      if(err) return console.log(err);
+      res.redirect("/");
+    });
+});
+ 
+// получем id редактируемого пользователя, получаем его из бд и отправлям с формой редактирования
+app.get("/edit/:id", function(req, res){
+  const id = req.params.id;
+  pool.query("SELECT * FROM _order WHERE o_id=?", [id], function(err, data) {
+    if(err) return console.log(err);
+     res.render("edit", {
+        order: data[0]
+    });
+  });
+});
+
+// получаем отредактированные данные и отправляем их в БД
+app.post("/edit", urlencodedParser, upload.single("filedata"), function (req, res) {
+         
+  if(!req.body) return res.sendStatus(400);
+
+  const id = req.body.id;
+  const date = req.body.date;
+  const client = req.body.client;
+  const status = req.body.status;
+  const sum = req.body.sum;
+
+  let fileinfo = req.file;
+  let file = "";
+
+  if (fileinfo)
+    file = fileinfo.path;
+   
+  pool.query("UPDATE _order SET o_date=?, o_client=?, o_status=?, o_sum=?, o_file=? WHERE o_id=?", [date, client, status, sum, file, id], function(err, data) {
+    if(err) return console.log(err);
+    res.redirect("/");
+  });
+});
+ 
+// получаем id удаляемого пользователя и удаляем его из бд
+app.post("/delete/:id", function(req, res){
+          
+  const id = req.params.id;
+  pool.query("DELETE FROM _order WHERE o_id=?", [id], function(err, data) {
+    if(err) return console.log(err);
+    res.redirect("/");
+  });
+});
+ 
+app.listen(3000, function(){
+  console.log("Сервер ожидает подключения...");
+});
