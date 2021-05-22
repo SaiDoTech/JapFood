f73847157850f5e716db76b3d1641e2b07b7cff6
Yaroslav Sidorkevich <72098175+SaiDoTech@users.noreply.github.com>
2021-05-22 18:46:10 +0300

    API realised

--- RestAPI/app.js
+++ RestAPI/app.js
@@ -2,24 +2,26 @@ const express = require("express");
 const mysql = require("mysql2");
 
 const bodyParser = require("body-parser");
+
 const multer  = require("multer");
 const upload = multer({dest:"uploads"});
 
 const app = express();
+const jsonParser = express.json();
 const urlencodedParser = bodyParser.urlencoded({extended: false})
 
 const pool = mysql.createPool({
     connectionLimit: 5,
     host: "localhost",
     user: "root",
-    database: "japfood",
+    database: "jap_order",
     password: "admin"
 });
 
 app.use(express.static(__dirname + "/public"));
 
 app.get("/api/orders", function(req, res){
-    pool.query("SELECT * FROM _order", function(err, data){
+    pool.query("SELECT * FROM jap_order.order", function(err, data){
         if (err) return res.sendStatus(404);
         res.send(data);
     })
@@ -28,38 +30,47 @@ app.get("/api/orders", function(req, res){
 app.get("/api/orders/:id", function(req, res){
     const id = req.params.id; // получаем id
 
-    pool.query("SELECT * FROM _order WHERE o_id=?", [id], function(err, data) {
+    pool.query("SELECT * FROM jap_order.order WHERE o_id=?", [id], function(err, data) {
         if(err) return res.sendStatus(404);
         res.send(data[0]);
     });
 });
 
-app.post("/api/orders", urlencodedParser, upload.single("filedata"), function (req, res){
+app.post("/api/orders", jsonParser, function (req, res){
     if(!req.body) return res.sendStatus(400);
 
-    const o_id = req.body.o_id;
-    const o_date = req.body.o_date;
-    const o_status = req.body.o_status;
-    const o_sum = req.body.o_sum;
-    const o_client = req.body.o_client;
+    const o_date = req.body.date;
+    const o_user = req.body.client;
+    const o_status = req.body.status;
+    const o_sum = req.body.sum;
 
-    let fileinfo = req.file;
-    let file = "";
-  
-    if (fileinfo)
-      file = fileinfo.path;
-
-    pool.query("UPDATE _order SET o_date=?, o_client=?, o_status=?, o_sum=?, o_file=? WHERE o_id=?", 
-                [o_date, o_client, o_status, o_sum, file, o_id], function(err, data) {
+    pool.query("INSERT INTO jap_order.order (o_date, o_user, o_status, o_sum) VALUES (?,?,?,?)", 
+                [o_date, o_user, o_status, o_sum], function(err, data) {
         if(err) return res.sendStatus(404);
-        res.send(date);
+        res.send(data);
     });
 });
+
+app.put("/api/orders", jsonParser, function (req, res){
+    if(!req.body) return res.sendStatus(400);
+
+    const o_id = req.body.id;
+    const o_date = req.body.date;
+    const o_user = req.body.client;
+    const o_status = req.body.status;
+    const o_sum = req.body.sum;
+
+    pool.query("UPDATE jap_order.order SET o_date=?, o_user=?, o_status=?, o_sum=? WHERE o_id=?",
+            [o_date, o_user, o_status, o_sum, o_id], function(err, data){
+        if(err) return res.sendStatus(404);
+        res.send(data);
+    });
+})
 
 app.delete("/api/orders/:id", function(req, res){
     const id = req.params.id;
 
-    pool.query("DELETE FROM _order WHERE o_id=?", [id], function(err, data) {
+    pool.query("DELETE FROM jap_order.order WHERE o_id=?", [id], function(err, data) {
       if(err) return res.sendStatus(404);
       res.send(id);
     });
